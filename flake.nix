5966404a9935112263d267ae03370ea452a285ff
Zoey de Souza Pessanha <zoey.pessanha@nubank.com.br>
2023-02-27 18:54:40 -0300

    feat: clona um repositório

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       };
 
       inputs = sys: with pkgs sys; [
-        rust-bin.stable.latest.default
+        rust-bin.stable.latest.default clippy
       ];
     in {
       devShells = {
--- src/service.rs
+++ src/service.rs
@@ -1,6 +1,7 @@
 use crate::{BackArgs, FrontArgs, Protocol};
-use anyhow::Result;
+use anyhow::{bail, Result};
 use convert_case::{Case, Casing};
+use std::path::Path;
 use std::{path::PathBuf, process::Command};
 use walkdir::WalkDir;
 
@@ -57,10 +58,10 @@ impl Service {
 fn get_repo_url(prog: Programming, ssh: bool) -> String {
     match (prog, ssh) {
         (Programming::Backend, true) => "git@github.com:peapescarte/backend_template".to_string(),
+        (Programming::Frontend, true) => "git@github.com:peapescarte/frontend_template".to_string(),
         (Programming::Backend, false) => {
             "https://github.com/peapescarte/backend_template".to_string()
         }
-        (Programming::Frontend, true) => "git@github.com:peapescarte/frontend_template".to_string(),
         (Programming::Frontend, false) => {
             "https://github.com/peapescarte/frontend_template".to_string()
         }
@@ -83,11 +84,18 @@ pub fn create_frontend_service(args: &FrontArgs) -> Result<()> {
 }
 
 fn clone_service(service: &Service) -> Result<()> {
-    Command::new("git")
+    let out = Command::new("git")
         .arg("clone")
         .arg(&service.repo)
         .arg(&service.path)
         .output()?;
+
+    // TODO
+
+    if !out.stderr.is_empty() {
+        println!("STDERR: {:?}", String::from_utf8(out.stderr)?);
+        return bail!("Failed to clone repository!");
+    };
 
     Ok(())
 }
@@ -108,7 +116,7 @@ fn apply_protocol_config(service: &Service, protocol: Protocol) -> Result<()> {
 fn rename_service(service: &Service) -> Result<()> {
     match service.prog {
         Programming::Backend => rename_elixir_project(&service.path, &service.name)?,
-        Programming::Frontend => rename_vue_project(&service.path, &service.name)?,
+        Programming::Frontend => rename_cljs_project(&service.path, &service.name)?,
     };
 
     Ok(())
@@ -117,12 +125,41 @@ fn rename_service(service: &Service) -> Result<()> {
 fn rename_elixir_project(path: &PathBuf, name: &String) -> Result<()> {
     let upper_camel_name = name.to_case(Case::UpperCamel);
 
-    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {}
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+        let content = std::fs::read_to_string(entry.path())?;
+
+        let rename = content
+            .as_str()
+            .replace("ServiceTemplate", &upper_camel_name)
+            .replace("service_template", name);
+
+        if entry
+            .file_name()
+            .to_str()
+            .unwrap()
+            .contains(&"service_template")
+        {
+            std::fs::remove_file(entry.path())?;
+
+            let path_renamed = entry
+                .path()
+                .as_os_str()
+                .to_str()
+                .unwrap()
+                .replace("service_template", name);
+
+            let path = Path::new(&path_renamed);
+
+            std::fs::write(path, rename)?;
+        } else {
+            std::fs::write(entry.path(), rename)?;
+        }
+    }
 
     Ok(())
 }
 
-fn rename_vue_project(path: &PathBuf, name: &String) -> Result<()> {
+fn rename_cljs_project(path: &PathBuf, name: &String) -> Result<()> {
     let camel_name = name.to_case(Case::Camel);
 
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {}
